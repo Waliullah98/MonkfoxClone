9d1fd3543f04d7c7d6020e2a8d19dce18f6943d3
Sayyed Waliullah <jiems2020@gmail.com>
2021-10-04 10:27:03 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,53 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ApplyComponent } from './apply/apply.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PlacementComponent } from './placement/placement.component';
+import { ProductsComponent } from './products/products.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",
+  component:HomeComponent
+  
+  },
+  
+  
+  {
+    path:'contact',
+    component:ContactComponent
+  },
+  {
+    path:'apply',
+    component:ApplyComponent
+
+  },
+  {
+    path:"about",
+    component:AboutComponent
+  },
+  {
+    path:'team',
+    component:TeamComponent
+  },
+  {
+    path:'catalog',
+    component:CatalogComponent
+
+  },
+  {
+    path:'products',
+    component:ProductsComponent
+  },
+  {
+    path:'placement',
+    component:PlacementComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './team/team.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ProductsComponent } from './products/products.component';
+import { PlacementComponent } from './placement/placement.component';
+import { ContactComponent } from './contact/contact.component';
+import { ApplyComponent } from './apply/apply.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    TeamComponent,
+    CatalogComponent,
+    ProductsComponent,
+    PlacementComponent,
+    ContactComponent,
+    ApplyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
